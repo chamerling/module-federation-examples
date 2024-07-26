eef5c1d2ac4372ab9f5a120b048e8634ba016091
Christophe Hamerling <christophe.hamerling@doctolib.com>
2024-07-26 14:54:58 +0200

    Update to latest rsbuild

--- react-manifest-example/remote1/rsbuild.config.ts
+++ react-manifest-example/remote1/rsbuild.config.ts
@@ -20,9 +20,9 @@ export default defineConfig({
             './button': './src/button.tsx',
             './app': './src/app.tsx',
           },
-          manifest: {
-            filePath: 'manifestpath',
-          },
+          //manifest: {
+          //  filePath: 'manifestpath',
+          //},
           shared: [
             'react',
             'react-dom',
